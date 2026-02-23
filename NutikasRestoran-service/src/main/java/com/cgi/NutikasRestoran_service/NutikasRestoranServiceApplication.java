43cc1e083e76522905dc58088b30c99829835347
tugilane <marten.peljo@gmail.com>
2026-02-23 13:09:33 +0200

    init repo

---
+++ NutikasRestoran-service/src/main/java/com/cgi/NutikasRestoran_service/NutikasRestoranServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cgi.NutikasRestoran_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutikasRestoranServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutikasRestoranServiceApplication.class, args);
+	}
+
+}
---
+++ NutikasRestoran-service/src/test/java/com/cgi/NutikasRestoran_service/NutikasRestoranServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.NutikasRestoran_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutikasRestoranServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
